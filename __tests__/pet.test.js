6e7d808cb319ece5bb176c9105cc2f0b1f341d3a
Tom Haynes <tomhaynes94@gmail.com>
2022-04-06 17:56:17 +0100

    set the name propety of the pet, set the pet's initial age as a property in an object, incremented the pet's age by 1 by adding a method to the pet object

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -2,7 +2,58 @@ describe("constructor", () => {
     it("returns an object", () => {
         expect(new Pet("Fido")).toBeInstanceOf(Object);
     });
+
+    it("sets the name property", () => {
+        const pet = {
+            name: "Fido"
+        };
+            expect(pet.name).toEqual("Fido");
+    });
+
+    it("pet has initial age of 0", () => {
+        const pet = new Pet ("Fido");
+        function Pet(name) {
+            this.name = name;
+            this.age = 0;
+        }
+            expect(pet.age).toEqual(0);
+    });
 });
+
+describe("growUp", () => {
+    it("increments Fido's age by 1", () => {
+        const pet = new Pet ("Fido");
+
+        function Pet(name) {
+            this.name = name;
+            this.age = 0;
+            this.growUp = function() {
+                this.age += 1;
+            };
+        }
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    });
+
+    it("increments Rex's age by 5", () => {
+        const pet = new Pet ("Rex");
+
+        function Pet(name) {
+            this.name = name;
+            this.age = 0;
+            this.growUp = function() {
+                this.age += 5;
+            };
+        }
+
+            pet.growUp();
+
+            expect(pet.age).toEqual(5);
+    });
+});
+
 
 function Pet () {}
 module.exports = Pet;
