0a8a4319ee15248180d780f76a7a1a611a068ad1
Tom Haynes <tomhaynes94@gmail.com>
2022-04-06 21:51:23 +0100

    fixed the code for the tests to work correctly; added prototypes to reduce pet's fitness and increases hunger as it ages and to give the pet exercise to increase its fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,36 +1,26 @@
+const Pet = require('../src/pet');
+
 describe("constructor", () => {
     it("returns an object", () => {
         expect(new Pet("Fido")).toBeInstanceOf(Object);
     });
 
     it("sets the name property", () => {
-        const pet = {
-            name: "Fido"
-        };
-            expect(pet.name).toEqual("Fido");
+        const pet = new Pet("Fido");
+
+        expect(pet.name).toEqual("Fido");
     });
 
     it("pet has initial age of 0", () => {
         const pet = new Pet ("Fido");
-        function Pet(name) {
-            this.name = name;
-            this.age = 0;
-        }
-            expect(pet.age).toEqual(0);
+
+        expect(pet.age).toEqual(0);
     });
 });
 
 describe("growUp", () => {
     it("increments Fido's age by 1", () => {
         const pet = new Pet ("Fido");
-
-        function Pet(name) {
-            this.name = name;
-            this.age = 0;
-            this.growUp = function() {
-                this.age += 1;
-            };
-        }
 
         pet.growUp();
 
@@ -40,20 +30,40 @@ describe("growUp", () => {
     it("increments Rex's age by 5", () => {
         const pet = new Pet ("Rex");
 
-        function Pet(name) {
-            this.name = name;
-            this.age = 0;
-            this.growUp = function() {
-                this.age += 5;
-            };
+        for (let i = 0; i < 5; i++) {
+            pet.growUp();
         }
 
-            pet.growUp();
+        expect(pet.age).toEqual(5);
 
-            expect(pet.age).toEqual(5);
+    });
+
+    it("Fido becomes hungrier by a factor of 5 and less fit by a factor of 3 as it gets older", () => {
+        const pet = new Pet("Fido");
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+        expect(pet.hunger).toEqual(5);
+    });
+
+describe("walk", () => {
+    it("increases fitness by 4", () => {
+        const pet = new Pet("Fido");
+      
+        pet.fitness = 4;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(8);
+        });
+
+        it('increases fitness by to a maximum of 10', () => {
+            const pet = new Pet('Fido');
+        
+            pet.fitness = 8;
+            pet.walk();
+        
+            expect(pet.fitness).toEqual(10);
+        });
     });
 });
-
-
-function Pet () {}
-module.exports = Pet;
--- src/pet.js
+++ src/pet.js
@@ -1,1 +1,28 @@
-const Pet = require('../src/pet');
+const maximumFitness = 10;
+
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.fitness = maximumFitness;
+    this.hunger = 0;
+};
+
+Pet.prototype.growUp = function () {
+    this.age +=1;
+
+    if (this.age >= 1) {
+        this.fitness = this.fitness - 3;
+        this.hunger = this.hunger + 5;
+    }
+};
+
+Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= maximumFitness ) {
+      this.fitness += 4;
+    } else {
+      this.fitness = maximumFitness;
+    }
+  }
+
+
+module.exports = Pet;
